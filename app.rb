a1315d9c4f7ec6b43100d49af55226b0d2529e35
Jake Epstein <James.Lewis.Epstein@gmail.com>
2016-06-23 19:24:14 -0700

    secret key env var

--- app.rb
+++ app.rb
@@ -3,9 +3,14 @@ require 'sinatra'
 class SlackSlashRoll < Sinatra::Base
   get '/' do
     "Stay awhile, and listen"
+    ENV["SLACK_SECRET_KEY"]
   end
 
   post '/roll' do
+    key = ENV["SLACK_SECRET_KEY"]
+
     "Rollin', rolling', rollin' haven't slept in weeks"
+
+
   end
 end
