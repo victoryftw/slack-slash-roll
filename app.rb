df250ac206cdb46835960f5eb9f2ac2d82fe5788
Jake Epstein <James.Lewis.Epstein@gmail.com>
2016-06-23 19:36:00 -0700

    test params parsing

--- app.rb
+++ app.rb
@@ -1,16 +1,19 @@
 require 'sinatra'
+require './lib/roller'
 
 class SlackSlashRoll < Sinatra::Base
   get '/' do
     "Stay awhile, and listen"
-    ENV["SLACK_SECRET_KEY"]
   end
 
   post '/roll' do
     key = ENV["SLACK_SECRET_KEY"]
 
+    if key != ""
     "Rollin', rolling', rollin' haven't slept in weeks"
+    end
 
+    params["channel_id"]
 
   end
 end
